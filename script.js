f64e421efae3adf08bee103a4d856a657e8692cb
Kevin Liu <liukproductivity@gmail.com>
2023-06-03 13:21:36 -0400

    Completed site for desktop generation

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 function changeBackgroundColor(color) {
   document.body.style.backgroundColor = color;
+  const outputContainer = document.getElementById("outputContainer");
+  outputContainer.style.backgroundColor = color;
 }
 
 function generateQRCode() {
-  var titleInput = document.getElementById("titleInput").value;
-  var descriptionInput = document.getElementById("descriptionInput").value;
-  var linkInput = document.getElementById("linkInput").value;
+  const titleInput = document.getElementById("titleInput").value;
+  const descriptionInput = document.getElementById("descriptionInput").value;
+  const linkInput = document.getElementById("linkInput").value;
 
-  var textContainer = document.getElementById("textContainer");
+  const textContainer = document.getElementById("textContainer");
   textContainer.innerHTML =
     "<h2>" + titleInput + "</h2>" + "<p>" + descriptionInput + "</p>";
 
-  var qrCodeContainer = document.getElementById("qrCodeContainer");
+  const qrCodeContainer = document.getElementById("qrCodeContainer");
   qrCodeContainer.innerHTML = "";
 
-  var qrText = linkInput;
+  const qrText = linkInput;
 
-  var qr = new QRCode(qrCodeContainer, {
+  const qr = new QRCode(qrCodeContainer, {
     text: qrText,
     width: 200,
     height: 200,
   });
 
-  var downloadLink = document.getElementById("downloadLink");
-  downloadLink.href = qrCodeContainer.firstChild.toDataURL("image/png");
+  html2canvas(document.getElementById("outputContainer")).then(function (
+    canvas
+  ) {
+    const outputContainer = document.getElementById("outputContainer");
+    outputContainer.classList.remove("hide-container");
+    outputContainer.classList.add("show-container");
+
+    // Rest of the code
+  });
 }
